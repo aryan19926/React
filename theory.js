5f4f97b12036fc7688131a646241bba6db064c3e
aryan19926 <aryanwork926@gmail.com>
2025-07-19 05:00:35 +0530

    react+parcel

---
+++ theory.js
@@ -0,0 +1,16 @@
+// bundlers --> webpack, vite, parcel
+
+// "devDependencies": {
+//     "parcel": "^2.15.4"  
+// }
+
+// ~version “Approximately equivalent to version”, 
+// will automatically update you to all future patch versions that are backwards-compatible, 
+// without incrementing the minor version. ~1.2.3 will use releases from 1.2.3 to < 1.3.0.
+
+// ^version “Compatible with version”, 
+// will automatically update you to all future minor/patch versions that are backwards-compatible, 
+// without incrementing the major version. ^1.2.3 will use releases from 1.2.3 to < 2.0.0.  
+
+// package.json is a file that contains the dependencies and scripts for a project.
+// package-lock.json is a file that contains the exact version of the dependencies that are installed in the node_modules folder.
