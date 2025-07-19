e8cc9294a309d7f6db538928b4ba242670844864
aryan19926 <aryanwork926@gmail.com>
2025-07-20 02:54:17 +0530

    food-app-header-added

--- app.js
+++ app.js
@@ -1,28 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = <h1>Hello JSX</h1>;
 
-const Heading2 = () => {
+// Header Component
+const Header = () => {
     return (
-        <h1>Hello JSX</h1>
+        <a href="/">
+            <div className="header">
+                <img className="burger-logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png" alt="logo" />
+                <ul className="nav-items">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </a>
+    );
+};
+//Body Component
+const Body = () => {
+    return (
+        <h1>Body</h1>
     );
 };
 
-// component composition --> we can use a component inside another component
-const HeadingComponent = () => {
+//Footer Component
+const Footer = () => {
     return (
-        <div>
-            {heading}
-            {Heading2()}
-            {/*{Heading2()} and <Heading2 /> are the same*/}
-            <Heading2 />
-            <h1>Hello JSX Functional</h1>
-        </div>
+        <h1>Footer</h1>
     );
-}
+};
+
+// AppLayout Component
+const AppLayout = () => {
+    return (
+        <React.Fragment>
+            <Header />
+            <Body />
+            <Footer />
+        </React.Fragment>
+    );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
 
 // React components --> 1. Functional 2. Class based
